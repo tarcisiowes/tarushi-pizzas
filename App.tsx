52dcd32d19ded59cbd6a9b94f09f984ba4a9319a
Tarcisio Wesley <tarcisiowesley@gmail.com>
2022-05-08 19:41:05 -0300

    build(app): status bar added + config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar'
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
 import { ThemeProvider } from 'styled-components/native'
@@ -20,6 +20,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme} >
+      <StatusBar style='light' translucent backgroundColor='transparent' />
       <SingIn />
     </ThemeProvider>
   );
