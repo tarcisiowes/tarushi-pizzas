eb928edbb87570038e519c537b727433a3d92c63
Tarcisio Wesley <tarcisiowesley@gmail.com>
2022-05-08 17:35:22 -0300

    perf(app): added theme provider to give global styles access to all pages

--- App.tsx
+++ App.tsx
@@ -3,7 +3,10 @@ import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
-  
+import { ThemeProvider } from 'styled-components/native'
+
+import theme from './src/theme'
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
@@ -15,8 +18,8 @@ export default function App() {
   }
 
   return (
-    <View >
+    <ThemeProvider theme={theme} >
       
-    </View>
+    </ThemeProvider>
   );
 }
