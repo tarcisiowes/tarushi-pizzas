fb13ffd6d9e5b43f09ccc5c5e9dca1d9bc10af6c
Tarcisio Wesley <tarcisiowesley@gmail.com>
2022-05-15 11:53:25 -0300

    feat(signin): added the new button

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 import { Container } from './styles';
 
 export function SignIn() {
@@ -16,6 +17,10 @@ export function SignIn() {
         placeholder="Senha"
         type="secondary"
         secureTextEntry
+      />
+      <Button
+        title="Entrar"
+        type="secondary"
       />
     </Container>
   );
