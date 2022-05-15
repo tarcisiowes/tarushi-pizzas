91f4b904a030f425ef26fcd7d37d834d6aa244d6
Tarcisio Wesley <tarcisiowesley@gmail.com>
2022-05-15 00:32:45 -0300

    feat(app): added an input for email and password

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import { Input } from '@components/Input';
 import { Container } from './styles';
 
 export function SignIn() {
   return (
-    <Container />
+    <Container>
+      <Input
+        placeholder="E-mail"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <Input
+        placeholder="Senha"
+        type="secondary"
+        secureTextEntry
+      />
+    </Container>
   );
 }
