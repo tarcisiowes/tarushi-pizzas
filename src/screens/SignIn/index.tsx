291876b9251af921b0225b3416d36dbc17b72c98
Tarcisio Wesley <tarcisiowesley@gmail.com>
2022-05-15 23:17:00 -0300

    feat(signin): added KeyboardAvoidingView + title + image + content

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
-import { Container } from './styles';
+import brandImage from '@assets/brand.png';
+import {
+  Brand, Container, Content, Title,
+} from './styles';
 
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
-      <Input
-        placeholder="Senha"
-        type="secondary"
-        secureTextEntry
-      />
-      <Button
-        title="Entrar"
-        type="secondary"
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <Content>
+          <Brand source={brandImage} />
+          <Title>Login</Title>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input
+            placeholder="Senha"
+            type="secondary"
+            secureTextEntry
+          />
+          <Button
+            title="Entrar"
+            type="secondary"
+          />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
