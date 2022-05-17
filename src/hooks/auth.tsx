1f197c2ff3919bc801a976f2241a2b83b3f67688
Tarcisio Wesley <tarcisiowesley@gmail.com>
2022-05-17 10:56:36 -0300

    feat(auth): created authentication with useContext

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,27 @@
+import React, { useContext, createContext, ReactNode } from 'react';
+
+type AuthContextData = {
+
+}
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  return (
+    <AuthContext.Provider value={{}}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
