fc359ab7c1d7607c610bd56043ac741e3ec4d1f9
Tarcisio Wesley <tarcisiowesley@gmail.com>
2022-05-17 13:06:50 -0300

    feat(auth): created func signin, auth by firebase and alerts

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,7 +1,15 @@
-import React, { useContext, createContext, ReactNode } from 'react';
+import React, {
+  useContext,
+  createContext,
+  ReactNode,
+  useState,
+} from 'react';
+import auth from '@react-native-firebase/auth';
+import { Alert } from 'react-native';
 
 type AuthContextData = {
-
+  signIn: (email: string, password: string) => Promise<void>
+  isLogging: boolean
 }
 
 type AuthProviderProps = {
@@ -11,8 +19,38 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
+  const [isLogging, setIsLogging] = useState(false);
+
+  async function signIn(email:string, password:string) {
+    if (!email || !password) {
+      return Alert.alert('Login', 'Informe o e-mail e senha.');
+    }
+    setIsLogging(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((account: JSON) => {
+        console.log(account);
+      })
+      .catch((error: { code: string }) => {
+        const { code } = error;
+
+        if (code === 'auth/user-not-found' || code === 'auth/wrong-password') {
+          return Alert.alert('Login', 'E-mail e/ou senha inválida.');
+        }
+        return Alert.alert('Login', 'Não foi possivel realizar o login.');
+      })
+      .finally(() => {
+        setIsLogging(false);
+      });
+  }
+
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{
+      signIn,
+      isLogging,
+    }}
+    >
       {children}
     </AuthContext.Provider>
   );
