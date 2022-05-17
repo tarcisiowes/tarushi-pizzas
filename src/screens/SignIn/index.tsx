71581e2746bbfc768f42dc2780260127dcd06351
Tarcisio Wesley <tarcisiowesley@gmail.com>
2022-05-17 13:08:27 -0300

    feat(signin): added a handleSignin and states needed

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, Platform } from 'react-native';
+
+import { useAuth } from '@hooks/auth';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
@@ -13,6 +15,14 @@ import {
 } from './styles';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { signIn, isLogging } = useAuth();
+
+  function handleSignin() {
+    signIn(email, password);
+  }
+
   return (
     <Container>
       <KeyboardAvoidingView
@@ -26,11 +36,13 @@ export function SignIn() {
             type="secondary"
             autoCorrect={false}
             autoCapitalize="none"
+            onChange={setEmail}
           />
           <Input
             placeholder="Senha"
             type="secondary"
             secureTextEntry
+            onChange={setPassword}
           />
           <ForgotPasswordButton>
             <ForgotPasswordLabel>
@@ -40,6 +52,8 @@ export function SignIn() {
           <Button
             title="Entrar"
             type="secondary"
+            onPress={handleSignin}
+            isLoading={isLogging}
           />
         </Content>
       </KeyboardAvoidingView>
