b12f18cbde34af47ee823e07259ba8db69ac8b6e
tarushi <tarcisiowesley@gmail.com>
2026-03-15 17:35:04 +0900

    feat(app): integrate splash screen and update font loading logic

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import AppLoading from 'expo-app-loading';
+import React, { useCallback } from 'react';
+import { View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
@@ -9,22 +10,32 @@ import { SignIn } from '@screens/SignIn';
 import { AuthProvider } from '@hooks/auth';
 import theme from './src/theme';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
     DMSerifDisplay_400Regular,
   });
 
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null;
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar style="light" translucent backgroundColor="transparent" />
-      <AuthProvider>
-        <SignIn />
-      </AuthProvider>
-    </ThemeProvider>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <ThemeProvider theme={theme}>
+        <StatusBar style="light" translucent backgroundColor="transparent" />
+        <AuthProvider>
+          <SignIn />
+        </AuthProvider>
+      </ThemeProvider>
+    </View>
   );
 }
